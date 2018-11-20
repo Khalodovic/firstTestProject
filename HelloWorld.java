e26180d4bc3e89e22a738dcc0546fd087363bffb
khalid <ennaser79@gmail.com>
2018-11-20 14:36:31 +0100

    commit ajout pull jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World and solo Khalid");
+        System.out.println("Hello, World and solo Khalid et son amour Meriem.");
     }
 
 }
